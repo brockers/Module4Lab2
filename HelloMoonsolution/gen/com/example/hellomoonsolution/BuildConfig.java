127d51aa008d2fed4e73765d4f8fc9420456dc06
Bob Rockers <bob@rockerssoft.org>
2015-05-19 18:55:45 -0500

    Final solutions.

---
+++ HelloMoonsolution/gen/com/example/hellomoonsolution/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.hellomoonsolution;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ HelloMoonsolution/src/com/example/hellomoonsolution/HelloMoonFragment.java
@@ -0,0 +1,49 @@
+package com.example.hellomoonsolution;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class HelloMoonFragment extends Fragment {
+	private Button mPlayButton;
+	private Button mStopButton;
+	private AudioPlayer mPlayer = new AudioPlayer();
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+		View v = inflater.inflate(R.layout.fragment_hello_moon, parent, false);
+		mPlayButton = (Button)v.findViewById(R.id.hellomoon_playbutton);
+		mStopButton = (Button)v.findViewById(R.id.hellomoon_stopbutton);
+		mPlayButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				mPlayer.play(getActivity());
+			}
+	
+		});
+		
+		mStopButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				mPlayer.stop();
+			}
+	
+		});
+		
+		return v;
+		
+	}
+	
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		mPlayer.stop();
+	}
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setRetainInstance(true);
+	}
+	}
